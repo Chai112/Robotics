3283ab2a9589e87171ca77b71efe0a5b592662d9
CuteLittleAtoms <ike.sintavanarong@gmail.com>
2020-10-07 15:38:22 +0700

    yeet

---
+++ vexcode-projects/InterfaceTest/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       lke Sintavanarong                                         */
+/*    Created:      Wed Oct 07 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
