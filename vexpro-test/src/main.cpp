53418522acd677e8b30ddd0f2825de10f6dd1b3b
Chai <sirdesignpie@gmail.com>
2020-10-10 17:10:36 +0700

    Add Arduino

---
+++ vexpro-test/src/main.cpp
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Chaidhat Chaimongkol                             */
+/*    Created:      Wed Sep 23 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor1               motor         1               
+// Optical2             optical       2               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
